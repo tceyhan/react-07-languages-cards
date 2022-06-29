56e96db1c31a914bc28e89eedd8def739087bc2c
tceyhan <55tc155@gmail.com>
2022-06-29 21:44:38 +0300

    card and item components  wth css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Card from './components/card/Card';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="main-container">
-      <h1>Language Card Project</h1>
+      <Header/>
+      <Card />
     </div>
   );
 }
