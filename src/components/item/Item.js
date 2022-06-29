b8e00d47a934b2b1ae59ad94cace4fdd92153362
tceyhan <55tc155@gmail.com>
2022-06-29 21:45:19 +0300

    card and item comp added

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,1 +1,16 @@
-import React from 'react';
+import React from 'react'
+import "./Item.css"
+
+const Item = ({card}) => {
+
+const { name, img, options } = card;
+  return (
+    <div className='card'>
+        <img className ="card-logo" src={img} alt="" />
+        <h3 className='card-title'>{name}</h3>      
+    </div>
+  )
+}
+
+export default Item
+
